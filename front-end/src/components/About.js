08e8cfb0ecfba82ccb1fd7620e526f73a6cd7627
Sam <hembysam4@gmail.com>
2020-03-05 10:16:57 -0600

    filling in views

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -1,8 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class About extends Component {
   render() {
-    return <div>Hello From About</div>;
+    return (
+      <div className="about f-co-c">
+        <h1 className="about-header">About Us</h1>
+        <div className="about-body">
+          <div className="about-image flo-l"></div>
+          <p className="about-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            hic nobis repellat rem quasi corporis, vero possimus beatae
+            reiciendis odio soluta dolorum officiis expedita culpa. Nam, harum.
+            Nisi, quae fuga? Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Officiis et vel quasi iste ex necessitatibus praesentium fugit
+            dolor possimus, quibusdam doloribus velit, est, cupiditate atque
+            inventore deleniti enim soluta at? Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Deserunt nesciunt, praesentium
+            architecto odio numquam natus.
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -1,8 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Contact extends Component {
   render() {
-    return <div>Hello From Contact</div>;
+    return (
+      <div className="contact f-c-c">
+        <form action="submit" className="contact-form f-co-c-sa">
+          <input type="text" className="form-name" />
+          <input type="text" className="form-email" />
+          <input type="text" className="form-phone" />
+          <input type="text" className="form-address" />
+          <textarea
+            name="message"
+            id=""
+            cols="30"
+            rows="10"
+            className="form-message"
+          ></textarea>
+          <input type="submit" className="form-submit" />
+        </form>
+      </div>
+    );
   }
 }
 
