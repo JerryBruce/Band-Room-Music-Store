d7a399273d3ecee84bed7dca89a7f6bb942da0b1
Sam <hembysam4@gmail.com>
2020-03-09 19:35:06 -0500

    deleted redux code

--- front-end/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = (menuOpen, action) => {
-  if (action.type === MENU_OPEN) {
-    return action.payload;
-  } else if (action.type === MENU_CLOSE) {
-    return action.payload;
-  }
-};
