f953986adec5ebdee716aacf9017bd1c52aafb73
Sam <hembysam4@gmail.com>
2020-03-17 10:23:36 -0500

    pull from master

--- front-end/src/actions/toggle.js
+++
@@ -1,7 +0,0 @@
-import { TOGGLE_MENU } from './types';
-
-export const toggle = () => {
-  return {
-    type: TOGGLE_MENU
-  };
-};
--- front-end/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import toggleReducer from './toggleReducer';
-
-export const rootReducer = combineReducers({
-  toggleReducer
-});
