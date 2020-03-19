ee5eb2b655076be325cf01d8d2b409c45142002f
Sam <hembysam4@gmail.com>
2020-03-18 20:50:43 -0500

    updated item route

--- routes/item.js
+++ routes/item.js
@@ -66,7 +66,6 @@ router.delete('/items/:id', auth, async (req, res) => {
       return res.status(404).send();
     }
 
-<<<<<<< HEAD
     res.send(`${item.name} has been deleted.`);
   } catch (e) {
     res.status(500).send();
@@ -74,20 +73,3 @@ router.delete('/items/:id', auth, async (req, res) => {
 });
 
 module.exports = router;
-=======
-const upload = multer({
-  limits: {
-    fileSize: 1000000
-  },
-    fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-        return cb(new Error('Please upload a picture'))
-        }
-        cb(undefined, true)
-    }
-})
-
-
-
-module.exports = router
->>>>>>> d2dc57f4d5b5a13aca2983958425ef891e64a25f
