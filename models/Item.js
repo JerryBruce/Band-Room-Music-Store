9b5dee5dd86975f4c2628ac158ce82e845cd847b
Sam <hembysam4@gmail.com>
2020-03-18 20:52:11 -0500

    updated item schema

--- models/Item.js
+++ models/Item.js
@@ -17,43 +17,6 @@ const itemSchema = new mongoose.Schema({
   },
   stock: {
     type: Number,
-    required: false
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  image: {
-    type: String,
-    required: false
-  },
-  rent: {
-    type: Number,
-    required: false
-  },
-  buy: {
-    type: Number,
-    required: false
-  }
-});
-const itemSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  brand: {
-    type: String,
-    trim: true
-  },
-  series: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  stock: {
-    type: Number,
     required: true
   },
   description: {
