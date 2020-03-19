8fd42cc0e51ca6e7b77d6e66bbaa00c058b2134e
Sam <hembysam4@gmail.com>
2020-03-18 20:46:09 -0500

    two models?

--- models/Item.js
+++ models/Item.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
 const itemSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -38,45 +37,43 @@ const itemSchema = new mongoose.Schema({
     required: false
   }
 });
-=======
-const itemSchema = new mongoose.Schema({ 
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    brand: {
-        type: String,
-        trim: true
-    },
-    series: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    stock: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    image: {
-        type: Buffer,
-        required: false
-    },
-    rent: {
-        type: Number,
-        required: true
-    },
-    buy: {
-        type: Number,
-        required: false
-    }
-})
->>>>>>> d2dc57f4d5b5a13aca2983958425ef891e64a25f
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  brand: {
+    type: String,
+    trim: true
+  },
+  series: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  stock: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: Buffer,
+    required: false
+  },
+  rent: {
+    type: Number,
+    required: true
+  },
+  buy: {
+    type: Number,
+    required: false
+  }
+});
 
 const Item = mongoose.model('Item', itemSchema);
 
