5c58534b91c24c0d7c34a5d881a4664bc94be896
Sam <hembysam4@gmail.com>
2020-03-19 22:52:17 -0500

    stylized the add item form

--- front-end/src/components/Admin/CreateItem.js
+++ front-end/src/components/Admin/CreateItem.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import { toggleCreate } from '../../state/actions/toggle';
 import { createItem } from '../../state/actions/items';
+import e from 'express';
 
 class CreateItem extends React.Component {
   constructor() {
@@ -10,7 +11,7 @@ class CreateItem extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(e) {
+  handleSubmit() {
     e.preventDefault();
     const item = {
       name: this.refs.name.value,
@@ -18,7 +19,6 @@ class CreateItem extends React.Component {
       series: this.refs.series.value,
       stock: this.refs.stock.value,
       description: this.refs.description.value,
-      image: this.refs.img.value,
       rent: this.refs.rent.value,
       buy: this.refs.buy.value
     };
@@ -28,24 +28,64 @@ class CreateItem extends React.Component {
 
   render() {
     return (
-      <div className='dash f-co-c-sa'>
+      <div className='create-item f-co-c-sa'>
         <form
-          className='f-co-c-sa'
+          className='create-form f-co-c-sa'
           action='submit'
           onSubmit={this.handleSubmit}>
-          <input type='text' ref='name' placeholder='Name' />
-          <input type='text' ref='brand' placeholder='brand' />
-          <input type='text' ref='series' placeholder='series' />
-          <input type='number' ref='stock' placeholder='stock' />
-          <textarea type='text' ref='description' placeholder='description' />
-          <input type='text' ref='img' placeholder='image URL' />
-          <input type='number' ref='rent' placeholder='rent price' />
-          <input type='number' ref='buy' placeholder='buy price' />
-          <button type='submit'>Submit</button>
+          <input
+            className='create-form-input'
+            type='text'
+            ref='name'
+            placeholder='Name'
+          />
+          <input
+            className='create-form-input'
+            type='text'
+            ref='brand'
+            placeholder='brand'
+          />
+          <input
+            className='create-form-input'
+            type='text'
+            ref='series'
+            placeholder='series'
+          />
+          <input
+            className='create-form-input'
+            type='number'
+            ref='stock'
+            placeholder='stock'
+          />
+          <textarea
+            className='create-form-desc'
+            type='text'
+            ref='description'
+            placeholder='description'
+          />
+          <input
+            className='create-form-input'
+            type='number'
+            ref='rent'
+            placeholder='rent price'
+          />
+          <input
+            className='create-form-input'
+            type='number'
+            ref='buy'
+            placeholder='buy price'
+          />
+          <div className='f-co-c-sb'>
+            <button className='create-form-btn btn-green' type='submit'>
+              Submit
+            </button>
+            <button
+              className='create-form-btn btn'
+              onClick={() => this.props.toggleCreate()}>
+              Back to Dashboard
+            </button>
+          </div>
         </form>
-        <button onClick={() => this.props.toggleCreate()}>
-          Back to Dashboard
-        </button>
       </div>
     );
   }
