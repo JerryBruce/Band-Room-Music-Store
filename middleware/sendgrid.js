aad8362a6e04a78e43abd09f475bc54eb4ce0e17
Sam <hembysam4@gmail.com>
2020-03-26 01:01:41 -0500

    destructuring refactor

--- middleware/sendgrid.js
+++ middleware/sendgrid.js
@@ -1,22 +1,23 @@
-const sgMail = require('@sendgrid/mail');
-const keys = require('../config/keys');
+const sgMail = require("@sendgrid/mail");
+const keys = require("../config/keys");
 
 sgMail.setApiKey(keys.sendgridKey);
 
-const sendInquiryEmail = (email, name, phone, address, message) => {
-    sgMail.send({
-      to: keys.email,
-      from: email,
-      subject: "Rental Inquiry",
-      text: `See the below customer details:
+const sendInquiryEmail = data => {
+  const { email, name, phone, address, message } = data;
+  sgMail.send({
+    to: keys.email,
+    from: email,
+    subject: "Rental Inquiry",
+    text: `See the below customer details:
 
             Name: ${name}
             Phone: ${phone}
             Mailing: ${address}
             Message: ${message}`
-    });
-}
+  });
+};
 
 module.exports = {
-    sendInquiryEmail
-}
+  sendInquiryEmail
+};
