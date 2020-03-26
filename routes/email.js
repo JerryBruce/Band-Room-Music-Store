315cd53da074c2d154c44da4f43a2656005910a0
Jerry Bruce <bruc3y33@gmail.com>
2020-03-25 22:11:42 -0500

    made inquiry forms required

--- routes/email.js
+++ routes/email.js
@@ -8,17 +8,17 @@ router.post('/inquiry', (req, res) => {
     const name = `${data.first} ${data.last}`
 
        try{
-        sendInquiryEmail(
-            data.email, 
-            name, 
-            data.phone,
-            data.address,
-            data.message
+            if (!data.email || !data.first || !data.last || !data.phone || !data.address || !data.message) {
+                res.status(400).send("Please enter the required fields");
+            }
+            sendInquiryEmail(
+                data.email, 
+                name, 
+                data.phone,
+                data.address,
+                data.message
         )
 
-        if(!data) {
-            res.status(400).send('There is nothing to send')
-        }
         res.status(201).send('Email Sent')
        } catch (e) {
            res.send(500).status()
